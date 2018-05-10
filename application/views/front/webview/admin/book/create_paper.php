75d17cc63aedbf70542a542faf98dc645d15061b
Martin SangDo <martin.sangdo@gmail.com>
2018-05-10 23:34:19 +0700

    - UI create new paper

--- application/views/front/webview/admin/book/create_paper.php
+++ application/views/front/webview/admin/book/create_paper.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>My paper list</title>
+    <title>Create new paper</title>
 
     <?php require_once(FCPATH.'application/views/front/webview/admin/common_head.php'); ?>
 
@@ -18,7 +18,24 @@
             <div class="g-pa-20">
                 <h2>Create new paper</h2>
                 <div class="table-responsive g-mb-40">
+                    <!-- form -->
+                    <div class="form-group g-mb-5">
+                        <label class="g-mb-5">Title (*)</label>
+                        <div class="g-pos-rel">
+                            <input id="" class="form-control form-control-md g-brd-gray-light-v7 g-brd-gray-light-v3--focus g-rounded-4 g-px-14 g-py-10" type="text" />
+                            <small class="g-font-weight-300 g-font-size-12 g-color-gray-dark-v6 g-pt-5">Length: <span id="">0</span> / 200</small>
+                        </div>
+                    </div>
+                    <div class="form-group g-mb-5">
+                        <label class="g-mb-5">Slug (*)</label>
+                        <div class="g-pos-rel">
+                            <input id="" class="form-control form-control-md g-brd-gray-light-v7 g-brd-gray-light-v3--focus g-rounded-4 g-px-14 g-py-10" type="text" />
+                            <small class="g-font-weight-300 g-font-size-12 g-color-gray-dark-v6 g-pt-5">Length: <span id="">0</span> / 250</small>
+                        </div>
+                    </div>
 
+
+                    <!-- end form -->
                 </div>
             </div>
             <?php require_once(FCPATH.'application/views/front/webview/admin/common_footer.php'); ?>
