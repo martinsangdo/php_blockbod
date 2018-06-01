2b83e7932bc2c91d4af2671864d6ff728481ae5c
Martin SangDo <martin.sangdo@gmail.com>
2018-06-01 15:35:58 +0700

    - UI footnotes

--- application/views/front/webview/blockbod/contact.php
+++ application/views/front/webview/blockbod/contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US" style="transform: none;">
 <head>
-    <title>Newsletter</title>
+    <title>Contact</title>
 
     <?php require_once 'common_head.php'; ?>
 </head>
@@ -24,17 +24,30 @@
                                 <div class="article-wrap-inner">
                                     <div class="content-wrap">
                                         <div class="content-wrap-inner">
-                                            <h1>Register newsletter</h1><br/><br/>
-                                            <h3>Keep up with blockchain and cryptocurrency technology trends with our insider reports.</h3>
-                                            <p>Simply enter your email address in the box below and select which newsletters you'd like to receive.</p>
-                                            <p><input type="text" class="w-300" id="" placeholder="name@email.com"/></p>
-                                            <ul>
-                                                <li><input id="mce-group[9685]-9685-0" checked="checked" name="group[9685][1]" type="checkbox" value="1"> <strong>Blockbod Weekly</strong> - Every Friday you'll get a curated run down of the 5 hottest topics.</li>
-                                                <li><input id="mce-group[9685]-9685-1" checked="checked" name="group[9685][2]" type="checkbox" value="2"> <strong>Daily News</strong> - Daily feed of all published Blockbod stories - Sent once per day.</li>
-                                                <li><input id="mce-group[9685]-9685-2" checked="checked" name="group[9685][16]" type="checkbox" value="16"> <strong>Blockbod Research</strong> - Be the first to know about our digital currency research.</li>
-                                                <li><input id="mce-group[9685]-9685-3" checked="checked" name="group[9685][32]" type="checkbox" value="32"> <strong>Blockbod Paper</strong> - Get exclusive offers to buy our papers.</li>
-                                            </ul>
-                                            <button type="button" class="">Subscribe</button>
+                                            <h3>We are welcome any your valuable feedback</h3>
+                                            <p>Your can email use via <a href="mailto:info@blockbod.com">contact@blockbod.com</a></p>
+
+                                            <h4>Or fill below information</h4>
+                                            <form>
+                                                <table border="0" class="tbl_no_border">
+                                                    <tr>
+                                                        <td width="20%">Your name (*)</td>
+                                                        <td><input type="text" class="w-300"/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Your email (*)</td>
+                                                        <td><input type="text" class="w-300"/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Content</td>
+                                                        <td><textarea rows="10"></textarea></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td></td>
+                                                        <td><button type="button" class="">Submit</button></td>
+                                                    </tr>
+                                                </table>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
---
+++ application/views/front/webview/blockbod/submit_contribution.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en-US" style="transform: none;">
+<head>
+    <title>Submit your contribution</title>
+
+    <?php require_once 'common_head.php'; ?>
+</head>
+
+<body class="post-template-default single single-post postid-2057 single-format-standard global-layout-right-sidebar blog-layout-grid global-sticky-sidebar" style="transform: none;">
+
+<div id="page" class="site" style="transform: none;">
+    <?php require_once 'common_menu.php'; ?>
+
+    <div id="content" class="site-content" style="transform: none;">
+
+
+        <div class="container" style="transform: none;">
+            <div class="inner-wrapper" style="transform: none;">
+                <div class="content-area" style="position: relative; overflow: visible; box-sizing: border-box; min-height: 1px;width:80%;">
+                    <!-- #main -->
+                    <div class="theiaStickySidebar" style="padding-top: 0px; padding-bottom: 1px; position: static; transform: none;">
+                        <main class="site-main" role="main">
+                            <article class="post-2057 post type-post status-publish format-standard has-post-thumbnail hentry category-fashion tag-fashion tag-girl tag-lifestyle">
+                                <div class="article-wrap-inner">
+                                    <div class="content-wrap">
+                                        <div class="content-wrap-inner">
+                                            <h3>Contribute your review & analysis articles</h3>
+                                            <p>We will review your document & contact back ASAP vi email <a href="mailto:info@blockbod.com">info@blockbod.com</a></p>
+
+                                            <h4>Please fill below information</h4>
+                                            <form>
+                                                <table border="0" class="tbl_no_border">
+                                                    <tr>
+                                                        <td width="20%">Your name (*)</td>
+                                                        <td><input type="text" class="w-300"/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Your email (*)</td>
+                                                        <td><input type="text" class="w-300"/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Your professional</td>
+                                                        <td><input type="text" class="w-300"/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Short description (*)</td>
+                                                        <td><textarea rows="10"></textarea></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Your file <small>(PDF/DOCX)</small></td>
+                                                        <td><input type="file" class=""/></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Some note to us</td>
+                                                        <td><textarea rows="3"></textarea></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td></td>
+                                                        <td><button type="button" class="">Submit</button></td>
+                                                    </tr>
+                                                </table>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </article><!-- #post-## -->
+                        </main>
+                    </div>
+                </div><!-- #primary -->
+
+
+                <div id="sidebar-primary" class="widget-area sidebar" role="complementary" style="position: relative; overflow: visible; box-sizing: border-box; min-height: 2050px;">
+                    <div class="theiaStickySidebar" style="padding-top: 0px; padding-bottom: 1px; position: absolute; transform: translateY(639px); width: 330px; top: 0px;">
+                    </div>
+                </div><!-- #secondary -->
+            </div><!-- .inner-wrapper -->
+        </div><!-- .container -->
+    </div><!-- #content -->
+
+
+    <?php require_once 'common_footer.php'; ?>
+
+</div><!-- #page -->
+</body></html>
+
+
+
+
+
